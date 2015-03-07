8c9fb504cbfe71a75c760fda9dfada2a3d2d1994
Philip Greenwald <philip@avidest.com>
2015-03-07 14:06:31 -0500

    made tweets create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,15 @@ class TweetsController < ApplicationController
 	def new
 		@tweet = Tweet.new
 	end
+
+	def create
+		@hello = "hello"
+		@tweet = Tweet.new(tweet_params)
+		@tweet.save
+		render 'new'
+	end
+
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end
 end
