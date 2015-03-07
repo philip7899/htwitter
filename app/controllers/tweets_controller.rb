99a67bd653da9c4b7d1423741f7487a188d07a6f
Philip Greenwald <philip@avidest.com>
2015-03-07 15:30:34 -0500

    created flash messages

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,10 +4,11 @@ class TweetsController < ApplicationController
 	end
 
 	def create
-		@hello = "hello"
 		@tweet = Tweet.new(tweet_params)
-		@tweet.save
-		render 'new'
+		if @tweet.save
+			flash[:success] = "You have created a tweet"
+			redirect_to new_tweet_path
+		end
 	end
 
 	def tweet_params
