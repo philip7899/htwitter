2120076e49e190faba2a4249d4854c0f12071ff2
Philip Greenwald <philip@avidest.com>
2015-03-08 12:22:01 -0400

    added create profile functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tweets
+
+  has_one :profile
 end
